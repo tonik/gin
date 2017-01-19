3db361c89780b01905f6d814f6dc9ae3975cc781
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2017-01-19 16:39:37 +0100

    Prepares PHPUnit

---
+++ tests/SampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+class SampleTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function it_should_be_true()
+    {
+        $this->assertEquals(true, true);
+    }
+}
