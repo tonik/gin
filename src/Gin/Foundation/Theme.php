089e547911708b39da3492f5736b61c98f510afd
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2017-05-19 16:30:00 +0200

    Changes and refactoring in theme container

--- src/Gin/Foundation/Theme.php
+++ src/Gin/Foundation/Theme.php
@@ -2,54 +2,98 @@
 
 namespace Tonik\Gin\Foundation;
 
+use Closure;
 use Tonik\Gin\Foundation\Exception\BindingResolutionException;
 
 class Theme extends Singleton
 {
     /**
-     * Theme registry.
+     * Collection of services.
      *
      * @var array
      */
-    protected $registry = [];
+    protected $services = [];
 
     /**
-     * Bind value into theme registry.
+     * Collection of services factories.
+     *
+     * @var array
+     */
+    protected $factories = [];
+
+    /**
+     * Registry of deposited services.
+     *
+     * @var array
+     */
+    protected $deposit = [];
+
+    /**
+     * Bind service into collection.
      *
      * @param  string $key
-     * @param  mixed $value
+     * @param  Closure $service
      *
      * @return void
      */
-    public function bind($key, $value)
+    public function bind($key, Closure $service)
     {
-        $this->registry[$key] = $value;
+        $this->services[$key] = $service;
 
         return $this;
     }
 
     /**
-     * Resolve value from theme registry.
+     * Bind factory into collection.
+     *
+     * @param  string $key
+     * @param  \Closure $factory
+     *
+     * @return void
+     */
+    public function factory($key, Closure $factory)
+    {
+        $this->factories[$key] = $factory;
+
+        return $this;
+    }
+
+    /**
+     * Resolve service form container.
      *
      * @param  string $key
      * @param  array $parameters
      *
      * @return mixed
      */
-    public function get($key, $parameters = [])
+    public function get($key, array $parameters = [])
     {
-        if (! isset($this->registry[$key])) {
-            throw new BindingResolutionException("Unresolvable resolution. The [{$key}] binding is not registered.");
+        // If service is a factory, we should always
+        // return new instance of the service.
+        if (isset($this->factories[$key])) {
+            return $this->resolve($this->factories[$key], $parameters);
         }
 
-        if (is_callable($abstract = $this->registry[$key])) {
-            if (is_array($parameters)) {
-                return call_user_func_array($abstract, $parameters);
+        if (isset($this->services[$key])) {
+            if (! isset($this->deposit[$key])) {
+                $this->deposit[$key] = $this->resolve($this->services[$key], $parameters);
             }
 
-            return call_user_func($abstract, $parameters);
+            return $this->deposit[$key];
         }
 
-        return $abstract;
+        throw new BindingResolutionException("Unresolvable resolution. The [{$key}] binding is not registered.");
+    }
+
+    /**
+     * Resolves service with parameters.
+     *
+     * @param  mixed $abstract
+     * @param  array  $parameters
+     * @return mixed
+     */
+    protected function resolve($abstract, array $parameters)
+    {
+         return call_user_func_array($abstract, [$this, $parameters]);
     }
 }
--- tests/Gin/Foundation/ThemeTest.php
+++ tests/Gin/Foundation/ThemeTest.php
@@ -17,11 +17,11 @@ class ThemeTest extends TestCase
     /**
      * @test
      */
-    public function test_binding_and_resolving_value_from_the_theme_container()
+    public function test_binding_and_resolving_service()
     {
         $theme = Theme::getInstance();
 
-        $theme->bind('key', 'value');
+        $theme->bind('key', function () { return 'value'; });
 
         $this->assertEquals($theme->get('key'), 'value');
     }
@@ -29,17 +29,49 @@ class ThemeTest extends TestCase
     /**
      * @test
      */
-    public function test_binding_and_resolving_callable_from_the_theme_container()
+    public function test_binding_and_resolving_service_with_params()
     {
         $theme = Theme::getInstance();
 
-        $theme->bind('callable.without.parameters', function () { return 'value'; });
-        $theme->bind('callable.with.parameter', function ($param) { return $param; });
-        $theme->bind('callable.with.parameters', function ($param1, $param2) { return $param1.','.$param2; });
+        $theme->bind('params', function ($theme, $params) { return $params; });
 
-        $this->assertEquals($theme->get('callable.without.parameters'), 'value');
-        $this->assertEquals($theme->get('callable.with.parameter', 'value'), 'value');
-        $this->assertEquals($theme->get('callable.with.parameters', ['value1', 'value2']), 'value1,value2');
+        $this->assertEquals(['name' => 'John'], $theme->get('params', ['name' => 'John']));
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_give_access_to_the_theme_container_instance()
+    {
+        $theme = Theme::getInstance();
+
+        $theme->bind('theme', function ($t) { return $t; });
+
+        $this->assertSame($theme, $theme->get('theme'));
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_return_same_object_on_binding()
+    {
+        $theme = Theme::getInstance();
+
+        $theme->bind('singleton', function () { return new stdClass; });
+
+        $this->assertSame($theme->get('singleton'), $theme->get('singleton'));
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_return_same_object_on_factory_binding()
+    {
+        $theme = Theme::getInstance();
+
+        $theme->factory('factory', function () { return new stdClass; });
+
+        $this->assertNotSame($theme->get('factory'), $theme->get('factory'));
     }
 
     /**
